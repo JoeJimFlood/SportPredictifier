6169f412d3d21a79972ae856de2fd9f43bc84f6d
JoeJimFlood <joejimflood@gmail.com>
2022-11-27 14:20:09 -0800

    Added team and stadium objects

---
+++ SportPredictifier/Stadium.py
@@ -0,0 +1,26 @@
+class Stadium:
+
+    def __init__(self, code, name, location, lat, lon):
+        """
+        Stadium where a game can be played
+
+        Attributes
+        ----------
+        code (str):
+            Code used for identifying team name
+        name (str):
+            Stadium name
+        location (str):
+            Stadium location
+        lat (float):
+            Latitude of stadium
+        lon (float):
+            Longitude of stadium
+        elev (float):
+            Elevation (in meters) of stadium
+        """
+        self.code = code
+        self.name = name
+        self.location = location
+        self.lat = lat
+        self.lon = lon
---
+++ SportPredictifier/Team.py
@@ -0,0 +1,24 @@
+class Team:
+
+    def __init__(self, code, name, stadium, color1, color2):
+        """
+        Team that participates in a competition
+
+        Attributes
+        ----------
+        code (str):
+            Code used to identify team
+        name (str):
+            Team name
+        stadium (SportPredictifier.Stadium):
+            Team's usual home stadium
+        color1 (str):
+            Hex codes for team's primary color
+        color2 (str):
+            Hex codes for team's secondary color
+        """
+        self.code = code
+        self.name = name
+        self.stadium = stadium
+        self.color1 = color1
+        self.color2 = color2

499525141b19245314872bf286b4c1a530c27c51
JoeJimFlood <joejimflood@gmail.com>
2022-11-27 14:04:49 -0800

    Added simulate.py

---
+++ SportPredictifier/simulate.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+def weighted_variance(data, weights):
+    assert len(data) == len(weights), 'Data and weights must be same length'
+    weighted_average = np.average(data, weights = weights)
+    v1 = weights.sum()
+    v2 = np.square(weights).sum()
+    return (weights*np.square(data - weighted_average)).sum() / (v1 - (v2/v1))

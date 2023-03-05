f5ee80341f55c32a7c8d485da441b17f04d4095b
JoeJimFlood <joejimflood@gmail.com>
2023-03-04 23:38:24 -0800

    Close #4

--- SportPredictifier/main.py
+++ SportPredictifier/main.py
@@ -5,11 +5,13 @@ from .report import generate_report
 from .util import create_score_tables
 
 def initialize_season():
+    print('Initializing Season')
     season_settings = settings('settings.yaml')
     (stadia, teams, score_settings) = data(season_settings, initializing_season = True)
     create_score_tables(season_settings, teams, stadia, score_settings)
 
 def predictify(round_number):
+    print('Predictifying Round {}'.format(round_number))
     season_settings = settings('settings.yaml')
     (stadia, teams, score_settings) = data(season_settings, round_number)
 
@@ -23,7 +25,7 @@ def predictify(round_number):
     generate_report(outfile, teams, results)
 
 def main():
-    if sys.argv[1] == 'initiliaze_season':
+    if sys.argv[1] == 'initialize_season':
         initialize_season()
     elif sys.argv[1] == 'predictify':
         predictify(int(sys.argv[2]))

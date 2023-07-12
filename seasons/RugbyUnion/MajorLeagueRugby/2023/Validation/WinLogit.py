f7b90c8dbf02c8ed869feea92744a7b3f2121689
JoeJimFlood <joejimflood@gmail.com>
2023-07-11 20:36:39 -0700

    Added 2023 MLR Validation

---
+++ seasons/RugbyUnion/MajorLeagueRugby/2023/Validation/WinLogit.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import os
+from scipy.special import expit, logit
+
+infile = 'WinLogitData8+.csv'
+data = pd.read_csv(infile)
+data['LogOdds'] = logit(data['HomeChance'])
+data['Intercept'] = np.ones_like(data.index)
+
+print(data)
+
+X = data[['LogOdds', 'Intercept']]
+y = data['HomeWin']
+
+reg = sm.Logit(y, X)
+res = reg.fit()
+print(res.summary())

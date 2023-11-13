7dde27e81eedd7a6e56415c0cdf18450f0d7517b
JoeJimFlood <joejimflood@gmail.com>
2023-11-13 13:26:19 -0800

    Added comments and cleaned up code

--- SportPredictifier/load.py
+++ SportPredictifier/load.py
@@ -121,7 +121,7 @@ def __load_score_tables(score_table_path, query = None, drop_null_score_table_re
     -------
     score_tables (SportPredictifier.ObjectCollection):
         Collection of score tables stored as `pandas.DataFrame` objects
-    '''score_tables
+    '''
     score_tables = ObjectCollection()
     for score_table_file in os.listdir(score_table_path):
         print("Loading score table for {}".format(score_table_file[:-4]))
@@ -139,23 +139,62 @@ def __load_score_tables(score_table_path, query = None, drop_null_score_table_re
     return score_tables
 
 def settings(settings_file):
+    '''
+    Loads a settings.yaml file into a dictionary containing the settings
+
+    Parameters
+    ----------
+    settings_file (str):
+        Filepath of settings file
+    
+    Returns
+    -------
+    data (dict):
+        Dictionary containing settings
+    '''
     with open(settings_file, 'r') as f:
         data = yaml.safe_load(f)
         f.close()
     return data
 
 def data(settings, round_number = None, score_table_query = None, drop_null_score_table_records = False, initializing_season = False):
+    '''
+    Loads data on the stadia, teams, and score settings into memory. `ObjectCollection` objects containing those objects are returned.
+    Spatial weights, team statistics, and opponent statitics are calculated upon loading.
+
+    Parameters
+    ----------
+    settings (dict):
+        Dictionary containing the settings of the competition to be modeled
+    round_number (int):
+        The round number that will be modeled. Required if spatial weights are being used
+    score_table_query (str):
+        Query to use when loading the score tables (must work when running `pandas.DataFrame.query()`)
+    drop_null_score_table_records (bool):
+        Indicates whether or not records containing null values in score tables should be dropped
+    initializing_season (bool):
+        If initializing the season, calculations aren't performed as the data doesn't yet exist
+
+    Returns
+    -------
+    stadia (SportPredictifier.ObjectCollection):
+        Collection of stadia to be used in the competition
+    teams (SportPredictifier.ObjectCollection):
+        Collection of teams competing in the competition
+    score_settings (SportPredictifier.ObjectCollection):
+        Collection of score_settings to be used in the competition
+    '''
     stadia = __load_stadia(settings['stadia_file'])
     teams = __load_teams(settings['teams_file'], stadia)
     score_settings = __load_score_settings(settings['score_settings_file'])
     score_tables = __load_score_tables(settings['score_table_path'], score_table_query, drop_null_score_table_records)
 
+    # If initializing season, no data yet exist to perform calculations on
     if not initializing_season:
         validate_score_tables(score_tables, score_settings, teams, stadia, settings)
 
-    if not initializing_season:
         if settings['use_spatial_weights']:
-            assert round_number is not None, "A round number is needed if calculating travel weights"
+            assert round_number is not None, "A round number is needed if calculating spatial weights"
             schedule = pd.read_csv(settings['schedule_file'])
             game_locations = {}
             for _, row in schedule.iterrows():
@@ -174,15 +213,40 @@ def data(settings, round_number = None, score_table_query = None, drop_null_scor
     return stadia, teams, score_settings
 
 def schedule(settings, teams, stadia, score_settings, round_number = None, multithreaded = False, result_dict = None, schedule_override = None):
+    '''
+    Loads the competition schedule from a CSV (or input data frame) into an `ObjectCollection` containing each of the games onto different threads
+    so they can be simulated in parallel.
+
+    Parameters
+    ----------
+    settings (dict):
+        Settings for the competition
+    teams (SportPredictifier.ObjectCollection):
+        Collection of teams competing in the competition
+    stadia (SportPredictifier.ObjectCollection):
+        Collection of stadia used in the competition
+    score_settings (SportPredictifier.ObjectCollection):
+        Collection of score settings used in the competision
+    round_number (int, optional):
+        If not `None`, only that rounds games will be loaded
+    multithreaded (bool):
+        If true, each game will be loaded onto a different thread and run in parallel
+    result_dict (dict):
+        Dictionary containing results of games. Required if `multithreaded` is `True`
+    schedule_override (pandas.DataFrame):
+        Schedule table to override the schedule file if not reading from a CSV
+    '''
     print("Loading schedule")
-    if schedule_override is None:
+
+    if schedule_override is None: # Read in schedule from CSV file defined in settings file
         if round_number is None:
             schedule_table = pd.read_csv(settings['schedule_file'])
         else:
             schedule_table = pd.read_csv(settings['schedule_file']).query('round_number == @round_number')
-    else:
+    else: # Override schedule with input data frame
         schedule_table = schedule_override.copy()
 
+    # Add schedule attributes
     schedule_table['n_simulations'] = settings['n_simulations']
     schedule_table['date'] = pd.to_datetime(schedule_table[["year", "month", "day"]])
     schedule_table['score_settings'] = schedule_table.shape[0]*[score_settings]

2897cb008c27485ca845ae40f8b32c95a4584667
JoeJimFlood <joejimflood@gmail.com>
2023-12-18 21:15:43 -0800

    Added 2023 NFL Week 16 forecasts

--- seasons/AmericanFootball/NationalFootballLeague/2023/Validation/WinLogit.py
+++ seasons/AmericanFootball/NationalFootballLeague/2023/Validation/WinLogit.py
@@ -3,9 +3,15 @@ import numpy as np
 import statsmodels.api as sm
 import os
 from scipy.special import expit, logit
+import sys
+
+if len(sys.argv) > 1:
+    min_week = int(sys.argv[1])
+else:
+    min_week = 1
 
 infile = 'WinLogitData8+.csv'
-data = pd.read_csv(infile).dropna()
+data = pd.read_csv(infile).dropna().query('Week >= @min_week')
 data['LogOdds'] = logit(data['HomeChance'])
 data['Intercept'] = np.ones_like(data.index)
 
